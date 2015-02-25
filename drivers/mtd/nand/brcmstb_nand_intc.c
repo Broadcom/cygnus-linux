99977477e728c5db289e296915b8604c5a11b52c
Brian Norris <computersforpeace@gmail.com>
2015-02-25 10:29:07 -0800

    brcmstb_nand_intc: add 63138 interrupt support
    Signed-off-by: Brian Norris <computersforpeace@gmail.com>

--- drivers/mtd/nand/brcmstb_nand_intc.c
+++ drivers/mtd/nand/brcmstb_nand_intc.c
@@ -27,8 +27,76 @@ struct brcmnand_intc_ofdata {
 	void (*ctlrdy_set_enabled)(struct brcmnand_intc *intc, bool en);
 };
 
+struct bcm63138_nand_intc_priv {
+	void __iomem *base;
+};
+
+#define BCM63138_NAND_INT_STATUS		0x00
+#define BCM63138_NAND_INT_EN			0x04
+
+enum {
+	BCM63138_CTLRDY		= BIT(4),
+};
+
+static bool bcm63138_nand_intc_ack(struct brcmnand_intc *intc)
+{
+	struct bcm63138_nand_intc_priv *priv = intc->priv;
+	void __iomem *mmio = priv->base + BCM63138_NAND_INT_STATUS;
+	u32 val = __raw_readl(mmio);
+
+	if (val & BCM63138_CTLRDY) {
+		__raw_writel(val & ~BCM63138_CTLRDY, mmio);
+		return true;
+	}
+
+	return false;
+}
+
+static void bcm63138_nand_intc_set(struct brcmnand_intc *intc, bool en)
+{
+	struct bcm63138_nand_intc_priv *priv = intc->priv;
+	void __iomem *mmio = priv->base + BCM63138_NAND_INT_EN;
+	u32 val = __raw_readl(mmio);
+
+	if (en)
+		val |= BCM63138_CTLRDY;
+	else
+		val &= ~BCM63138_CTLRDY;
+
+	__raw_writel(val, mmio);
+}
+
+static int bcm63138_nand_intc_init(struct brcmnand_intc *intc)
+{
+	struct bcm63138_nand_intc_priv *priv;
+
+	priv = devm_kzalloc(intc->dev, sizeof(*priv), GFP_KERNEL);
+	if (!priv)
+		return -ENOMEM;
+
+	priv->base = of_io_request_and_map(intc->dn, 0, DRV_NAME);
+	if (IS_ERR(priv->base))
+		return PTR_ERR(priv->base);
+
+	intc->priv = priv;
+
+	/* Clear the interrupt */
+	bcm63138_nand_intc_ack(intc);
+
+	return 0;
+}
+
+static const struct brcmnand_intc_ofdata bcm63138_nand_intc = {
+	.init			= bcm63138_nand_intc_init,
+	.ctlrdy_ack		= bcm63138_nand_intc_ack,
+	.ctlrdy_set_enabled	= bcm63138_nand_intc_set,
+};
+
 static const struct of_device_id brcmnand_intc_ofmatch[] = {
-	/* TODO: fill in */
+	{
+		.compatible	= "brcm,nand-intc-bcm63138",
+		.data		= &bcm63138_nand_intc,
+	},
 	{},
 };
 
